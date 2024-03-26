a53e720f8ae82170987baad81ac0ee135c53c7e8
starcaptainarvay <avyaytheguitarist@gmail.com>
2024-03-26 02:59:16 -0400

    Moved Vulkan surface initializers to vulkan-utils.c

--- src/obrien.c
+++ src/obrien.c
@@ -1,6 +1,5 @@
 #include "obrien.h"
-// #include "video_manager.h"
-// #include "vulkan_utils.h"
+#include "vulkan_utils.h"
 
 #define GLFW_INCLUDE_VULKAN
 #define EXIT_FAILURE -1
@@ -10,71 +9,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-VkSurfaceKHR createSurface(VkInstance instance, GLFWwindow* window) {
-    VkSurfaceKHR surface;
-    if (glfwCreateWindowSurface(instance, window, NULL, &surface) != VK_SUCCESS) {
-        fprintf(stderr, "Failed to create window surface\n");
-        exit(EXIT_FAILURE);
-    }
-    return surface;
-}
-
-VkInstance createInstance() {
-    // Specify application info
-    VkApplicationInfo appInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
-    appInfo.pApplicationName = "Transient";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
-
-    // Specify instance creation info
-    VkInstanceCreateInfo createInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
-    createInfo.pApplicationInfo = &appInfo;
-
-    // Get required instance extensions
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
-
-    // Create Vulkan instance
-    VkInstance instance;
-    if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
-        fprintf(stderr, "Failed to create Vulkan instance\n");
-        exit(EXIT_FAILURE);
-    }
-
-    return instance;
-}
-
 void runApplication() {
-    // initVideoManager();
-    // loadVideo("SilentAssembly_3-8.mp4");
-    // initVulkan();
-    // createRenderPass();
-    // createGraphicsPipeline();
-
-
-    // renderFrame();
-
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initialize GLFW\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
     if (!primaryMonitor) {
         fprintf(stderr, "Unable to get primary monitor.");
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     const GLFWvidmode* videoMode = glfwGetVideoMode(primaryMonitor);
     if (!videoMode) {
         fprintf(stderr, "Unable to get video mode.");
         glfwTerminate();
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     /*
@@ -88,7 +40,8 @@ void runApplication() {
     if (!window) {
         fprintf(stderr, "Failed to create GLFW window\n");
         glfwTerminate();
-        return -1;
+        
+        exit(EXIT_FAILURE);
     }
 
     // Create Vulkan instance
@@ -110,8 +63,6 @@ void runApplication() {
     vkDestroyInstance(instance, NULL);
     glfwDestroyWindow(window);
     glfwTerminate();
-
-    return 0;
 }
 
 int main() {
--- src/vulkan_utils.h
+++ src/vulkan_utils.h
@@ -1,9 +1,18 @@
 #ifndef VULKAN_UTILS_H
 #define VULKAN_UTILS_H
 
+#define GLFW_INCLUDE_VULKAN
+#define EXIT_FAILURE 1
+
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
 // Function prototypes related to Vulkan utilities
 void initVulkan();
 void createGraphicsPipeline();
 void createRenderPass();
+
+VkSurfaceKHR createSurface(VkInstance instance, GLFWwindow* window);
+VkInstance createInstance();
 
 #endif
