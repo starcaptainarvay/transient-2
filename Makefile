f39b4beef023f223f8337205d3b7283a3bcdb9c6
starcaptainarvay <avyaytheguitarist@gmail.com>
2024-03-26 03:25:16 -0400

    Cleaned up code

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ CC = gcc
 CFLAGS = -Wall -Wextra -I.
 LDFLAGS = -lglfw -lvulkan -lavformat -lavcodec -lavutil -lswscale
 
-SRCS = src/vulkan_utils.c src/video_player.c src/video_manager.c src/obrien.c
+# SRCS = src/vulkan_window.c src/video_player.c src/video_manager.c src/obrien.c
+SRCS = src/vulkan_window.c src/obrien.c
 OBJS = $(SRCS:.c=.o)
 EXEC = Transient.exe
 
--- src/obrien.c
+++ src/obrien.c
@@ -1,56 +1,15 @@
 #include "obrien.h"
-#include "vulkan_utils.h"
-
-#define GLFW_INCLUDE_VULKAN
-#define EXIT_FAILURE -1
+#include "vulkan_window.h"
 
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void runApplication() {
-    if (!glfwInit()) {
-        fprintf(stderr, "Failed to initialize GLFW\n");
-        exit(EXIT_FAILURE);
-    }
-
-    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-    if (!primaryMonitor) {
-        fprintf(stderr, "Unable to get primary monitor.");
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    const GLFWvidmode* videoMode = glfwGetVideoMode(primaryMonitor);
-    if (!videoMode) {
-        fprintf(stderr, "Unable to get video mode.");
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    /*
-        Disable OpenGL binding for GLFW window.
-        See: https://stackoverflow.com/questions/73991660/vulkan-raii-got-vk-error-native-window-in-use-khr-when-trying-to-create-vksurfa
-    */
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); 
-
     // Create GLFW window
-    GLFWwindow* window = glfwCreateWindow(videoMode->width, videoMode->height, "Transient", primaryMonitor, NULL);
-    if (!window) {
-        fprintf(stderr, "Failed to create GLFW window\n");
-        glfwTerminate();
-        
-        exit(EXIT_FAILURE);
-    }
+    TransientWindowState* state = createTransientWindow();
 
-    // Create Vulkan instance
-    VkInstance instance = createInstance();
-
-    // Create Vulkan surface
-    VkSurfaceKHR surface = createSurface(instance, window);
-
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(state->window)) {
         glfwPollEvents();
 
         // Rendering with Vulkan
@@ -59,10 +18,8 @@ void runApplication() {
     }
 
     // Clean up
-    vkDestroySurfaceKHR(instance, surface, NULL);
-    vkDestroyInstance(instance, NULL);
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    cleanupTransientWindow(state);
+    free(state);
 }
 
 int main() {
